3645761785a321fc933df811b5d13634e1b7bb09
ERICK JHONE <erick@sistemaquantum.com.br>
2024-10-30 18:26:44 -0300

    update

--- app/src/main/java/com/example/quizapp/activities/SportActivity.java
+++ app/src/main/java/com/example/quizapp/activities/SportActivity.java
@@ -2,18 +2,20 @@ package com.example.quizapp.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.quizapp.R;
 
 public class SportActivity extends AppCompatActivity {
 
+    private RadioGroup radioGroupFoundationYear;
+    private RadioGroup radioGroupBrazilianTitles;
+    private RadioGroup radioGroupTopScorer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,51 @@ public class SportActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sport);
 
+        radioGroupFoundationYear = findViewById(R.id.radioGroupFoundationYear);
+        radioGroupBrazilianTitles = findViewById(R.id.radioGroupBrazilianTitles);
+        radioGroupTopScorer = findViewById(R.id.radioGroupTopScorer);
 
+        setupListeners();
+    }
 
+    private void setupListeners() {
+        radioGroupFoundationYear.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                String selectedText = selectedRadioButton.getText().toString();
+                if (selectedText.equals("1910")) {
+                    Toast.makeText(SportActivity.this, "Acertou!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(SportActivity.this, "Errado!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        radioGroupBrazilianTitles.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                String selectedText = selectedRadioButton.getText().toString();
+                if (selectedText.equals("7")) {
+                    Toast.makeText(SportActivity.this, "Acertou!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(SportActivity.this, "Errado!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        radioGroupTopScorer.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                String selectedText = selectedRadioButton.getText().toString();
+                if (selectedText.equals("Ronaldo Fenômeno")) {
+                    Toast.makeText(SportActivity.this, "Acertou!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(SportActivity.this, "Errado!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
